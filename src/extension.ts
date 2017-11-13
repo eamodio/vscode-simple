58c9c322336dab7431782bba57a7f8156fe65d48
Eric Amodio <eamodio@gmail.com>
2017-11-13 17:37:22 -0500

    Bug #38229 - https://github.com/Microsoft/vscode/issues/38229

--- src/extension.ts
+++ src/extension.ts
@@ -2,4 +2,29 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
+    vscode.workspace.onDidChangeConfiguration((e: vscode.ConfigurationChangeEvent) => {
+        for (const folder of vscode.workspace.workspaceFolders!) {
+            try {
+                vscode.workspace.getConfiguration('files', folder.uri).get('exclude');
+            } catch (e) {
+                console.log(e, folder.uri.fsPath);
+            }
+        }
+    });
+    vscode.workspace.onDidChangeWorkspaceFolders((e: vscode.WorkspaceFoldersChangeEvent) => {
+        if (e.added.length) {
+            try {
+                vscode.workspace.getConfiguration('files', e.added[0].uri).get('exclude');
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        if (e.removed.length) {
+            try {
+                vscode.workspace.getConfiguration('files', e.removed[0].uri).get('exclude');
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    });
 }
