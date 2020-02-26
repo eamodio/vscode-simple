81c3246947e1d748b6a41fe9e54675f4108d5b65
Eric Amodio <eamodio@gmail.com>
2020-02-26 11:27:55 -0500

    Updates

--- src/extension.ts
+++ src/extension.ts
@@ -1,83 +1,172 @@
 'use strict';
 import * as vscode from 'vscode';
+import { GitExtension } from './git';
 
-export async function activate(context: vscode.ExtensionContext) {
-    const tree = new TestTree();
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand('simple.views.refresh', () => tree.refresh()),
-        vscode.commands.registerCommand('simple.views.toggle', (node: TestNode) => {
-            if (node.counter === undefined) {
-                node.counter = 0;
-            }
-            node.counter++;
-            if (node.collapsibleState === vscode.TreeItemCollapsibleState.None) {
-                node.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed;
-            } else {
-                node.collapsibleState = vscode.TreeItemCollapsibleState.None;
-            }
-            tree.refresh(node);
-        }),
-        vscode.window.registerTreeDataProvider('simple.view', tree)
-    );
+export function activate(context: vscode.ExtensionContext) {
+	// const gitExtension = await vscode.extensions.getExtension<GitExtension>('vscode.git')?.activate();
+	// if (gitExtension?.enabled !== true) {
+	// 	throw new Error('vscode.git Git extension not enabled');
+	// }
+	// const gitApi = gitExtension.getAPI(1);
+	// gitApi.onDidOpenRepository(async repo => {
+	// 	try {
+	// 		const log = await repo.log({ maxEntries: 10 });
+	// 		console.log(log.length);
+	// 	} catch (ex) {
+	// 		console.error(ex);
+	// 	}
+	// });
 }
 
-class TestTree implements vscode.TreeDataProvider<TestNode> {
-    protected _onDidChangeTreeData = new vscode.EventEmitter<TestNode>();
-    get onDidChangeTreeData(): vscode.Event<TestNode> {
-        return this._onDidChangeTreeData.event;
-    }
+// export async function activate(context: vscode.ExtensionContext) {
+// 	context.subscriptions.push(
+// 		vscode.workspace.registerTimelineProvider(
+// 			{},
+// 			{
+// 				source: 'my-magical-timeline-provider',
+// 				sourceDescription: 'My magical timeline provider',
+// 				provideTimeline: (uri: vscode.Uri, token: vscode.CancellationToken) => {
+// 					console.log('provide');
+// 					const result: vscode.TimelineItem[] = [new vscode.TimelineItem('rawr', Date.now())];
+// 					return result;
+// 				}
+// 			}
+// 		)
+// 	);
+// }
 
-    private _children: TestNode[] = [
-        new TestNode('Click to Refresh All', undefined, 'simple.views.refresh'),
-        new TestNode('Click to Toggle & Refresh Node', 0, 'simple.views.toggle', [new TestNode('Child')])
-    ];
+// export async function activate(context: vscode.ExtensionContext) {yarn
+// 	vscode.authentication.onDidRegisterAuthenticationProvider(async () => {
+// 		console.log('onDidRegisterAuthenticationProvider');
+// 		for (const provider of vscode.authentication.providers) {
+// 			console.log(provider.displayName, provider.id);
 
-    getTreeItem(element: TestNode): vscode.TreeItem | Thenable<vscode.TreeItem> {
-        return element;
-    }
+// 			// const sessions = await provider.getSessions();
+// 			// for (const session of sessions) {
+// 			// 	console.log(session.displayName);
+// 			// 	console.log(session.id);
+// 			// 	console.log(`scopes: ${session.scopes.join(', ')}`);
+// 			// }
+// 		}
+// 	});
 
-    getChildren(element?: TestNode): vscode.ProviderResult<TestNode[]> {
-        return element ? element.getChildren() : this._children;
-    }
+// 	vscode.authentication.onDidUnregisterAuthenticationProvider(async () => {
+// 		console.log('onDidUnregisterAuthenticationProvider');
+// 		for (const provider of vscode.authentication.providers) {
+// 			console.log(provider.displayName, provider.id);
 
-    refresh(node?: TestNode) {
-        this._onDidChangeTreeData.fire(node);
-    }
-}
+// 			// const sessions = await provider.getSessions();
+// 			// for (const session of sessions) {
+// 			// 	console.log(session.displayName);
+// 			// 	console.log(session.id);
+// 			// 	console.log(`scopes: ${session.scopes.join(', ')}`);
+// 			// }
+// 		}
+// 	});
 
-class TestNode implements vscode.TreeItem {
-    public collapsibleState: vscode.TreeItemCollapsibleState = vscode.TreeItemCollapsibleState.None;
+// 	const provider = new (class implements vscode.AuthenticationProvider {
+// 		id = 'GitHub';
+// 		displayName = 'GitHub';
 
-    constructor(
-        public id: string,
-        public counter?: number,
-        private _command?: string,
-        private _children: TestNode[] = []
-    ) {}
+// 		private _onDidChangeSessions = new vscode.EventEmitter<void>();
+// 		onDidChangeSessions: vscode.Event<void> = this._onDidChangeSessions.event;
 
-    get label(): string {
-        return this.id;
-    }
+// 		private _sessions: vscode.Session[] = [];
 
-    get description(): string {
-        return this.counter === undefined
-            ? ''
-            : `${this.counter}, state=${
-                  this.collapsibleState === vscode.TreeItemCollapsibleState.None ? 'none' : 'collapsed'
-              }`;
-    }
-    get command(): vscode.Command | undefined {
-        if (this._command === undefined) return undefined;
+// 		getSessions(): Promise<readonly vscode.Session[]> {
+// 			return Promise.resolve([...this._sessions]);
+// 			// throw new Error('Method not implemented.');
+// 		}
 
-        return {
-            title: this._command,
-            command: this._command,
-            arguments: [this]
-        };
-    }
+// 		login(scopes: string[]): Promise<vscode.Session> {
+// 			const session = {
+// 				id: this._sessions.length.toString(),
+// 				displayName: 'foo',
+// 				accessToken: 'bar',
+// 				scopes: scopes
+// 			};
+// 			this._sessions.push(session);
+// 			this._onDidChangeSessions.fire();
+// 			return Promise.resolve({ ...session });
+// 		}
 
-    getChildren(): vscode.ProviderResult<TestNode[]> {
-        return this._children;
-    }
-}
+// 		logout(sessionId: string): Promise<void> {
+// 			const index = this._sessions.findIndex(s => s.id === sessionId);
+// 			if (index !== -1) {
+// 				this._sessions.splice(index, 1);
+// 				this._onDidChangeSessions.fire();
+// 			}
+// 			return Promise.resolve();
+// 		}
+// 	})();
+// 	const disposable = vscode.authentication.registerAuthenticationProvider(provider);
+
+// 	for (const provider of vscode.authentication.providers) {
+// 		console.log(provider.displayName, provider.id);
+// 	}
+
+// 	// setTimeout(() => disposable.dispose(), 2000);
+
+// 	const session = await provider.login(['foo', 'bar']);
+// 	// session.
+// }
+
+// class TestTree implements vscode.TreeDataProvider<TestNode> {
+// 	protected _onDidChangeTreeData = new vscode.EventEmitter<TestNode>();
+// 	get onDidChangeTreeData(): vscode.Event<TestNode> {
+// 		return this._onDidChangeTreeData.event;
+// 	}
+
+// 	private _children: TestNode[] = [
+// 		new TestNode('Click to Refresh All', undefined, 'simple.views.refresh'),
+// 		new TestNode('Click to Toggle & Refresh Node', 0, 'simple.views.toggle', [new TestNode('Child')])
+// 	];
+
+// 	getTreeItem(element: TestNode): vscode.TreeItem | Thenable<vscode.TreeItem> {
+// 		return element;
+// 	}
+
+// 	getChildren(element?: TestNode): vscode.ProviderResult<TestNode[]> {
+// 		return element ? element.getChildren() : this._children;
+// 	}
+
+// 	refresh(node?: TestNode) {
+// 		this._onDidChangeTreeData.fire(node);
+// 	}
+// }
+
+// class TestNode implements vscode.TreeItem {
+// 	public collapsibleState: vscode.TreeItemCollapsibleState = vscode.TreeItemCollapsibleState.None;
+
+// 	constructor(
+// 		public id: string,
+// 		public counter?: number,
+// 		private _command?: string,
+// 		private _children: TestNode[] = []
+// 	) {}
+
+// 	get label(): string {
+// 		return this.id;
+// 	}
+
+// 	get description(): string {
+// 		return this.counter === undefined
+// 			? ''
+// 			: `${this.counter}, state=${
+// 					this.collapsibleState === vscode.TreeItemCollapsibleState.None ? 'none' : 'collapsed'
+// 			  }`;
+// 	}
+// 	get command(): vscode.Command | undefined {
+// 		if (this._command === undefined) return undefined;
+
+// 		return {
+// 			title: this._command,
+// 			command: this._command,
+// 			arguments: [this]
+// 		};
+// 	}
+
+// 	getChildren(): vscode.ProviderResult<TestNode[]> {
+// 		return this._children;
+// 	}
+// }
