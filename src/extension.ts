4dba60d90d556ac16a99837072183ccf0cb063c0
Eric Amodio <eamodio@gmail.com>
2020-11-19 11:54:59 -0500

    sync-keys

--- src/extension.ts
+++ src/extension.ts
@@ -1,57 +1,21 @@
 'use strict';
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
-	const tdp = new SimpleTree();
-	vscode.window.createTreeView('simple.view', { treeDataProvider: tdp });
-	vscode.commands.registerCommand('simple.command', () => tdp.refresh());
-}
+export function activate(context: vscode.ExtensionContext): void {
+	context.subscriptions.push(
+		vscode.commands.registerCommand('simple.setTrue', () => context.globalState.update('syncKey', true)),
+		vscode.commands.registerCommand('simple.setFalse', () => context.globalState.update('syncKey', false)),
+		vscode.commands.registerCommand('simple.setUndefined', () => context.globalState.update('syncKey', undefined))
+	);
 
-class SimpleTree implements vscode.TreeDataProvider<vscode.TreeItem> {
-	private _onDidChangeTreeData = new vscode.EventEmitter<vscode.TreeItem | null | undefined>();
-	onDidChangeTreeData = this._onDidChangeTreeData.event;
+	context.globalState.setKeysForSync(['syncKey']);
 
-	children: vscode.TreeItem[];
-
-	constructor() {
-		this.children = [
-			this.getChild('Compare', 'git-compare'),
-			this.getChild('Push', 'cloud-upload', 'simple.iconColor1'),
-			this.getChild('Pull', 'cloud-download', 'simple.iconColor2'),
-			this.getChild('Push', 'cloud-upload', 'simple.iconColor2'),
-			this.getChild('Pull', 'cloud-download', 'simple.iconColor1')
-		];
-
-		setInterval(() => {
-			const child = this.children[random(0, 4)];
-			child.label = `${child.contextValue} ${Date.now()}`;
-			this.refresh(child);
-		}, 1000);
-	}
-
-	refresh(item?: vscode.TreeItem) {
-		this._onDidChangeTreeData.fire(item);
-	}
-
-	getTreeItem(element: vscode.TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
-		if (element) return element;
-
-		const item = new vscode.TreeItem('root', vscode.TreeItemCollapsibleState.Expanded);
-		return item;
-	}
-
-	getChildren(element?: vscode.TreeItem): vscode.ProviderResult<vscode.TreeItem[]> {
-		return this.children;
-	}
-
-	private getChild(label: string, icon: string, color?: string) {
-		const item = new vscode.TreeItem(label, vscode.TreeItemCollapsibleState.None);
-		item.iconPath = new (vscode.ThemeIcon as any)(icon, color ? new vscode.ThemeColor(color) : undefined);
-		item.contextValue = label;
-		return item;
-	}
-}
-
-function random(min: number, max: number) {
-	return Math.floor(Math.random() * (max - min + 1) + min);
+	let disposable: vscode.Disposable | undefined;
+	setInterval(() => {
+		disposable?.dispose();
+		disposable = vscode.window.setStatusBarMessage(
+			// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+			`syncKey=${context.globalState.get<boolean>('syncKey')}, ${new Date()}`
+		);
+	}, 1000);
 }
