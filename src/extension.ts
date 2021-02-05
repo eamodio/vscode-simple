55885a60d30cad930f0ed91ac0d2733993fdaa6f
Eric Amodio <eamodio@gmail.com>
2021-02-05 10:41:39 -0500

    Diff with left/right

--- src/extension.ts
+++ src/extension.ts
@@ -2,20 +2,12 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext): void {
+	const fn = (command: string) => {
+		void vscode.window.showInformationMessage(`${command} executed`);
+	};
 	context.subscriptions.push(
-		vscode.commands.registerCommand('simple.setTrue', () => context.globalState.update('syncKey', true)),
-		vscode.commands.registerCommand('simple.setFalse', () => context.globalState.update('syncKey', false)),
-		vscode.commands.registerCommand('simple.setUndefined', () => context.globalState.update('syncKey', undefined))
+		vscode.commands.registerCommand('simple.command', () => fn('simple.command')),
+		vscode.commands.registerCommand('simple.commandInDiffLeft', () => fn('simple.commandInDiffLeft')),
+		vscode.commands.registerCommand('simple.commandInDiffRight', () => fn('simple.commandInDiffRight'))
 	);
-
-	context.globalState.setKeysForSync(['syncKey']);
-
-	let disposable: vscode.Disposable | undefined;
-	setInterval(() => {
-		disposable?.dispose();
-		disposable = vscode.window.setStatusBarMessage(
-			// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-			`syncKey=${context.globalState.get<boolean>('syncKey')}, ${new Date()}`
-		);
-	}, 1000);
 }
