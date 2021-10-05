c681132b8749a33d91add6abffbda892e838e737
Eric Amodio <eamodio@gmail.com>
2021-10-05 01:19:03 -0400

    fs-events

--- src/extension.ts
+++ src/extension.ts
@@ -1,64 +1,105 @@
 'use strict';
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
+	const fs = new RevisionFileSystem();
+
 	context.subscriptions.push(
-		vscode.commands.registerCommand('simple.value', () => {
-			const qp = vscode.window.createQuickPick();
-			qp.items = [
-				{ label: '1', description: '1' },
-				{ label: '2', description: '2' },
-				{ label: '3', description: '3' },
-				{ label: '4', description: '4' },
-			];
-
-			const timer = setInterval(() => {
-				qp.value = Math.floor(Math.random() * qp.items.length).toString();
-			}, 2500);
-
-			qp.onDidHide(() => clearInterval(timer));
-			qp.onDidChangeActive((e) => console.log('onDidChangeActive', e[0].label));
-			qp.onDidChangeSelection((e) => console.log('onDidChangeSelection', e[0].label));
-			qp.onDidChangeValue((e) => console.log('onDidChangeValue', e));
-			qp.show();
+		vscode.commands.registerCommand('simple.open', async () => {
+			vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('simple:///foo.md'));
 		}),
-		vscode.commands.registerCommand('simple.active', () => {
-			const qp = vscode.window.createQuickPick();
-			qp.items = [
-				{ label: '1', description: '1' },
-				{ label: '2', description: '2' },
-				{ label: '3', description: '3' },
-				{ label: '4', description: '4' },
-			];
 
-			const timer = setInterval(() => {
-				qp.activeItems = [qp.items[Math.floor(Math.random() * qp.items.length)]];
-			}, 2500);
-
-			qp.onDidHide(() => clearInterval(timer));
-			qp.onDidChangeActive((e) => console.log('onDidChangeActive', e[0].label));
-			qp.onDidChangeSelection((e) => console.log('onDidChangeSelection', e[0].label));
-			qp.onDidChangeValue((e) => console.log('onDidChangeValue', e));
-			qp.show();
+		vscode.commands.registerCommand('simple.reset', async () => {
+			fs.reset();
 		}),
-		vscode.commands.registerCommand('simple.selected', () => {
-			const qp = vscode.window.createQuickPick();
-			qp.items = [
-				{ label: '1', description: '1' },
-				{ label: '2', description: '2' },
-				{ label: '3', description: '3' },
-				{ label: '4', description: '4' },
-			];
 
-			const timer = setInterval(() => {
-				qp.selectedItems = [qp.items[Math.floor(Math.random() * qp.items.length)]];
-			}, 2500);
-
-			qp.onDidHide(() => clearInterval(timer));
-			qp.onDidChangeActive((e) => console.log('onDidChangeActive', e[0].label));
-			qp.onDidChangeSelection((e) => console.log('onDidChangeSelection', e[0].label));
-			qp.onDidChangeValue((e) => console.log('onDidChangeValue', e));
-			qp.show();
-		})
+		vscode.workspace.registerFileSystemProvider('simple', fs)
 	);
+}
+
+class RevisionFileSystem implements vscode.FileSystemProvider {
+	private state: { mtime: number; revision: number } = { mtime: Date.now(), revision: 1 };
+
+	reset() {
+		this.state = { mtime: Date.now(), revision: 1 };
+		this.canFastForward = true;
+	}
+
+	private canFastForward = true;
+	private fastForward() {
+		if (!this.canFastForward) return;
+
+		this.canFastForward = false;
+		this.state.mtime = Date.now();
+		this.state.revision++;
+		this._onDidChangeFile.fire([
+			{ type: vscode.FileChangeType.Changed, uri: vscode.Uri.parse('simple:///foo.md') },
+		]);
+	}
+
+	private _onDidChangeFile = new vscode.EventEmitter<vscode.FileChangeEvent[]>();
+	get onDidChangeFile(): vscode.Event<vscode.FileChangeEvent[]> {
+		return this._onDidChangeFile.event;
+	}
+
+	watch(uri: vscode.Uri, options: { recursive: boolean; excludes: string[] }): vscode.Disposable {
+		return {
+			dispose: () => {},
+		};
+	}
+
+	async stat(uri: vscode.Uri): Promise<vscode.FileStat> {
+		const state = { ...this.state };
+		console.log('stat', uri.toString(), state.revision);
+
+		await sleep(2500);
+		this.fastForward();
+		await sleep(2500);
+
+		return Promise.resolve({
+			type: vscode.FileType.File,
+			ctime: 0,
+			mtime: state.mtime,
+			size: 1,
+		});
+	}
+
+	async readFile(uri: vscode.Uri): Promise<Uint8Array> {
+		const state = { ...this.state };
+		console.log('readFile', uri.toString(), state.revision);
+
+		await sleep(2500);
+		this.fastForward();
+		await sleep(2500);
+
+		return Promise.resolve(Buffer.from(`revision: ${state.revision}`));
+	}
+
+	readDirectory(uri: vscode.Uri): [string, vscode.FileType][] {
+		throw new Error('Method not implemented.');
+	}
+
+	writeFile(uri: vscode.Uri, content: Uint8Array): void {
+		throw new Error('Method not implemented.');
+	}
+
+	createDirectory(uri: vscode.Uri): void {
+		throw new Error('Method not implemented.');
+	}
+
+	delete(uri: vscode.Uri, options: { recursive: boolean }): void {
+		throw new Error('Method not implemented.');
+	}
+
+	rename(oldUri: vscode.Uri, newUri: vscode.Uri, options: { overwrite: boolean }): void {
+		throw new Error('Method not implemented.');
+	}
+
+	copy(source: vscode.Uri, destination: vscode.Uri, options: { overwrite: boolean }): void {
+		throw new Error('Method not implemented.');
+	}
+}
+
+function sleep(ms: number) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
