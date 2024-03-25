61c818e3127f63f2a1b8d1fad244be1af54c0b5b
Eric Amodio <eamodio@gmail.com>
2024-03-25 19:02:28 -0400

    Demonstrates webview hang

--- src/extension.ts
+++ src/extension.ts
@@ -1,64 +1,112 @@
-import type {ExtensionContext} from "vscode";
-import { commands,  window } from "vscode";
+import type { CancellationToken, Disposable, ExtensionContext, Progress } from 'vscode';
+import { commands, ProgressLocation, ViewColumn, window } from 'vscode';
 
 export function activate(context: ExtensionContext) {
 	context.subscriptions.push(
-		commands.registerCommand('simple.value', () => {
-			const qp = window.createQuickPick();
-			qp.items = [
-				{ label: '1', description: '1' },
-				{ label: '2', description: '2' },
-				{ label: '3', description: '3' },
-				{ label: '4', description: '4' },
+		commands.registerCommand('simple.webview.open', () => {
+			const panel = window.createWebviewPanel('simple.sample', 'Simple Sample', ViewColumn.Active, {
+				enableScripts: true,
+				localResourceRoots: [],
+				retainContextWhenHidden: true,
+			});
+
+			panel.webview.html = `<!doctype html>
+<html>
+	<head>
+		<style type="text/css">
+			body, html { width: 100%; height: 100%; }
+			section { width: 100%; height: 100%; }
+			#console { width: 100%; height: 100%; }
+		</style>
+		<script type="text/javascript">
+			window.addEventListener('message', event => {
+				const el = document.getElementById('console');
+				el.value = \`\${el.value || ''}\n\${new Date().toISOString()}\t\${event.data.text}\`;
+			});
+		</script>
+	</head>
+	<body>
+		<section>
+			<h1>Simple Webview Sample</h1>
+			<textarea id="console"></textarea>
+		</section>
+	</body>
+</html>`;
+
+			function postMessage(text: string) {
+				return window.withProgress(
+					{ location: ProgressLocation.Notification, cancellable: true },
+					async (progress: Progress<{ message?: string; increment?: number }>, token: CancellationToken) => {
+						progress.report({ message: `Sending message: ${text}` });
+
+						let timeout: ReturnType<typeof setTimeout> | undefined;
+
+						// It looks like there is a bug where `postMessage` can sometimes just hang infinitely. Not sure why, but ensure we don't hang forever
+						const promise = Promise.race<boolean>([
+							panel.webview.postMessage({ text: text }).then(
+								s => {
+									clearTimeout(timeout);
+									return s;
+								},
+								ex => {
+									clearTimeout(timeout);
+									debugger;
+									void window.showErrorMessage(`Error sending message: ${ex}`);
+									return false;
+								},
+							),
+							new Promise<boolean>(resolve => {
+								token.onCancellationRequested(() => resolve(false));
+
+								timeout = setTimeout(() => {
+									debugger;
+									void window.showErrorMessage(`Timed out sending message: ${text}`);
+									resolve(false);
+								}, 15000);
+							}),
+						]);
+
+						const success = await promise;
+						return success;
+					},
+				);
+			}
+
+			const disposables: Disposable[] = [
+				panel.onDidDispose(() => {
+					// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+					disposables.forEach(d => d.dispose());
+				}),
+
+				// NOTE: Uncomment these lines to see that these `postMessage` calls will hang indefinitely right at the start
+
+				// panel.onDidChangeViewState(e => {
+				// 	void postMessage(
+				// 		`panel.onDidChangeViewState: active=${e.webviewPanel.active}, visible=${e.webviewPanel.visible}`,
+				// 	);
+				// }),
+				// window.onDidChangeWindowState(e => {
+				// 	void postMessage(`window.onDidChangeWindowState: focused=${e.focused}`);
+				// }),
 			];
 
-			const timer = setInterval(() => {
-				qp.value = Math.floor(Math.random() * qp.items.length).toString();
-			}, 2500);
+			panel.reveal();
 
-			qp.onDidHide(() => clearInterval(timer));
-			qp.onDidChangeActive(e => console.log('onDidChangeActive', e[0].label));
-			qp.onDidChangeSelection(e => console.log('onDidChangeSelection', e[0].label));
-			qp.onDidChangeValue(e => console.log('onDidChangeValue', e));
-			qp.show();
-		}),
-		commands.registerCommand('simple.active', () => {
-			const qp = window.createQuickPick();
-			qp.items = [
-				{ label: '1', description: '1' },
-				{ label: '2', description: '2' },
-				{ label: '3', description: '3' },
-				{ label: '4', description: '4' },
-			];
+			// NOTE: If you uncomment the lines above, comment these out
 
-			const timer = setInterval(() => {
-				qp.activeItems = [qp.items[Math.floor(Math.random() * qp.items.length)]];
-			}, 2500);
-
-			qp.onDidHide(() => clearInterval(timer));
-			qp.onDidChangeActive(e => console.log('onDidChangeActive', e[0].label));
-			qp.onDidChangeSelection(e => console.log('onDidChangeSelection', e[0].label));
-			qp.onDidChangeValue(e => console.log('onDidChangeValue', e));
-			qp.show();
-		}),
-		commands.registerCommand('simple.selected', () => {
-			const qp = window.createQuickPick();
-			qp.items = [
-				{ label: '1', description: '1' },
-				{ label: '2', description: '2' },
-				{ label: '3', description: '3' },
-				{ label: '4', description: '4' },
-			];
-
-			const timer = setInterval(() => {
-				qp.selectedItems = [qp.items[Math.floor(Math.random() * qp.items.length)]];
-			}, 2500);
-
-			qp.onDidHide(() => clearInterval(timer));
-			qp.onDidChangeActive(e => console.log('onDidChangeActive', e[0].label));
-			qp.onDidChangeSelection(e => console.log('onDidChangeSelection', e[0].label));
-			qp.onDidChangeValue(e => console.log('onDidChangeValue', e));
-			qp.show();
+			// Avoid setting up the event listeners until after reveal as `postMessage`'s called too early will hang indefinitely
+			setTimeout(() => {
+				disposables.push(
+					panel.onDidChangeViewState(e => {
+						void postMessage(
+							`panel.onDidChangeViewState: active=${e.webviewPanel.active}, visible=${e.webviewPanel.visible}`,
+						);
+					}),
+					window.onDidChangeWindowState(e => {
+						void postMessage(`window.onDidChangeWindowState: focused=${e.focused}`);
+					}),
+				);
+			}, 100);
 		}),
 	);
 }
